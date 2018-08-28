704ba5c78f516bc8c3f5d2edade8b375e0a42ca5
runtu <huorunquan@126.com>
2018-08-28 10:31:01 +0800

    NEW: 增加 view 和 service 的示例，但是 https://hacker-news.firebaseio.com/v0 这个接口不通

--- config/config.default.js
+++ config/config.default.js
@@ -1,1 +1,13 @@
 exports.keys = 'asd'
+// 添加view配置
+exports.view = {
+  defaultViewEngine: 'nunjucks',
+  mapping: {
+    '.tpl': 'nunjucks',
+  },
+}
+// the config of news
+exports.news = {
+  pageSize: 5,
+  serverUrl: 'https://hacker-news.firebaseio.com/v0',
+}
